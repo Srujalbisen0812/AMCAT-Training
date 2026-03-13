c05e646925e13a5c4eb3e00167495b2ad118e37c
Srujal Bisen <srujalbisen08@gmail.com>
2026-03-13 22:31:41 +0530

    Add files via upload

---
+++ Day 4.py
@@ -0,0 +1,77 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+day 4
+mydict = {
+...     101: "prashant",
+...     102: "ashish",
+...     "103": "mohini",
+...     "104":"trivani"
+...     }
+... print(mydict)
+... print(mydict[101])
+... for x in mydict.values():
+...     print(x)
+... ------------------------------------------------------------------------------------------------------------------------------------------
+... mydict = {
+...     101: "prashant",
+...     102: "ashish",
+...     "103": "mohini",
+...     "104":"trivani"
+...     }
+... print(mydict)
+... print(mydict[101])
+... for x in mydict.values():
+...     print(x)
+... 
+... for x, y in mydict.items():
+...     print(x, y)
+... -----------------------------------------------------------------------------------------------------------------------------------------
+... 
+... 
+... name = "racecar"
+... if name == name[::-1]:
+...     print("Palindrome")
+... else:   print("Not a palindrome")
+... 
+... ------------------------------------------------------------------------------------------------------------------------------------------
+... i = 2, j = 4
+... for i in range (2,5):
+...     for j in range (4,7):
+...         print(i,end=" ")
+...     print()
+
+
+------------------------------------------------------------------------------------------------------------------------------------------
+n= (input("enter a number: "))
+n = int(n)
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(chr(64+i),end=" ")
+    print()
+-----------------------------------------------------------------------------------------------------------------------------
+
+n=int (input("enter a number: "))
+for i in range(1,n+1):
+    for j in range(1,1+1):
+        print(i,end=" ")
+    print()
+
+
+------------------------------------------------------------------------------------------------------------------------------------------
+n= int (input("Enter number of students"))
+d={}
+for _ in range(n):
+    name=input("Enter student name: ")
+    marks=int(input("Enter marks: "))
+    d[name]=marks
+while True:
+    name=input("Enter student name to get marks")
+    markes=d.get(name,-1)
+    if markes != -1:
+        print("student not found")
+    else:
+        print("marks of",name,"are",markes)
+        options=input("Do you want to continue? (yes/no)")
+        if options.lower() != "yes":
+            break
+    print("Thank you for using the program") 
