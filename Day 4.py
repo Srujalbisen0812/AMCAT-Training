69d3bf55059a9c646cd419e6e68112d3f7ba681b
Srujal Bisen <srujalbisen08@gmail.com>
2026-03-13 22:33:07 +0530

    Update Day 4.py

--- Day 4.py
+++ Day 4.py
@@ -1,5 +1,4 @@
-Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+
 day 4
 mydict = {
 ...     101: "prashant",
